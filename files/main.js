1f8aaacfd2113acfe7d9fa34be0af944e36ab473
geo-cat <48271752+geo-cat@users.noreply.github.com>
2019-03-06 22:19:43 +0900

    update

--- files/main.js
+++ files/main.js
@@ -1,6 +1,6 @@
-function header(){
+﻿function header(){
     $.ajax({
-        url: "/files/head.html", // 読み込むHTMLファイル
+        url: "header.html", // 読み込むHTMLファイル
         cache: false,
         success: function(html){
             document.write(html);
